fd198dca4a57a4d57ba9ad7108ca7e41d844ee9d
Manly Man <manly.man@icloud.com>
2023-04-08 10:30:09 +0400

    dev

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { value NestFactory } from '@nestjs/core';
+import { value AppModule } from './app.module';
+import {
+  value FastifyAdapter,
+  value NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const adapter = new FastifyAdapter();
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    adapter,
+  );
+  app.enableCors();
+  app.enableShutdownHooks();
   await app.listen(3000);
 }
 bootstrap();
